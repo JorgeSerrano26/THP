41b5580a2b24770305bfb718036102f5d9b71bb4
JorgeSerrano26 <entrevistasmias@gmail.com>
2020-03-25 17:21:12 -0300

    primer ejercicio

---
+++ src/tp1/ejercicio1.java
@@ -0,0 +1,24 @@
+package tp1;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+	static Scanner input;
+
+	public static void main(String args[]) {
+
+
+		int num1 = 0;
+		int resultado1;
+		int resultado2;
+
+		resultado1 = num1 + 2;
+
+		System.out.println("El resultado 1 es " + resultado1);
+
+		resultado2 = resultado1 * resultado1;
+
+		System.out.println("El resultado 2 es " + resultado2);
+	}
+}
