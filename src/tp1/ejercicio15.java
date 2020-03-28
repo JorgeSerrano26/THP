ada3f4836cc236e929c07e5c83de69c28d66c7f1
JorgeSerrano26 <entrevistasmias@gmail.com>
2020-03-28 16:31:25 -0300

    Ejercicio 15 terminado

---
+++ src/tp1/ejercicio15.java
@@ -0,0 +1,24 @@
+package tp1;
+
+import java.util.Scanner;
+
+public class ejercicio15 {
+	static Scanner input;
+
+	public static void main(String[] args) {
+		/*
+		 * Existen dos reglas que identifican dos conjuntos de valores: 
+		 * - El número es de un solo dígito. 
+		 * - El número es impar. A partir de estas reglas, creá un
+		 * algoritmo que asigne a las variables booleanas 
+		 * estaEnA, estaEnB, estaEnAmbos y noEstaEnNinguno 
+		 * el valor verdadero o falso, según corresponda, para indicar
+		 * si el valor ingresado pertenece al primer conjunto, al segundo, a ambos o a
+		 * ninguno, respectivamente. Definí un lote de prueba de varios números y probá
+		 * el algoritmo, escribiendo los resultados.
+		 */
+		input = new Scanner(System.in);
+		input.close();
+		System.out.print(9-10);
+	}
+}
